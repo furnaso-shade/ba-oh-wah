053947dba1f64b016e5abccca6d2faafd98d8342
ziraffe <132818624+furnaso-shade@users.noreply.github.com>
2026-07-08 09:48:09 +0530

    Add files via upload

---
+++ kunafapancakes.py
@@ -0,0 +1,4 @@
+userdecision = input("did you eat kunafa pancakes today? ").strip().lower()
+
+print("HAHA motherfucker go suffer the consequences of your SHITASS life decisions HAHA" if userdecision == "yes" else "good girl")
+
